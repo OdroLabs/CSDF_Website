6f043751b1d46e985276fb8b17102b56e30e8d84
Lakshitha Perera <lakshitha1629@gmail.com>
2026-08-01 10:13:58 +0530

    fixed home page issue

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -261,7 +261,7 @@ export default async function HomePage({ params }: { params: { locale: Locale } 
             )}
           </div>
 
-          <Curve className="absolute inset-x-0 bottom-0 z-10 text-background" />
+          <Curve className="absolute inset-x-0 -bottom-px z-10 text-background" />
         </section>
       )}
 
